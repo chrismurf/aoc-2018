1efa70ca8c8903716f5de937d1427f2dd29795ed
Chris Murphy <chrismurf@gmail.com>
2021-12-11 10:34:14 -0500

    Cleanup of Day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,31 +2,28 @@ use std::collections::HashSet;
 use std::fs;
 use std::io::{self, prelude::*};
 
-fn main() -> io::Result<()> {
-    let file = fs::File::open("input.txt").unwrap();
-    let reader = io::BufReader::new(file);
-    let sequence: Vec<i32> = reader.lines().map(|line| line.unwrap().parse().unwrap()).collect();
-
+fn find_first_duplicate_frequency(sequence: &Vec<i32>) -> i32 {
     let mut frequency = 0;
     let mut frequencies_seen: HashSet<i32> = HashSet::from([0]);
-
-    for &delta in &sequence {
-        frequency += delta;
-    }
-    println!("Frequency after first 100 runs: {}", frequency);
-
-    frequency = 0;
-    'outer: loop {
-        for &delta in &sequence {
+    loop {
+        for &delta in sequence {
             frequency += delta;
             if frequencies_seen.contains(&frequency) {
-                println!("First duplicate frequency found: {}", frequency);
-                break 'outer;
+                return frequency;
             } else {
                 frequencies_seen.insert(frequency);
             }
         }
     }
+}
+
+fn main() -> io::Result<()> {
+    let file = fs::File::open("input.txt").unwrap();
+    let reader = io::BufReader::new(file);
+    let sequence: Vec<i32> = reader.lines().map(|line| line.unwrap().parse().unwrap()).collect();
+
+    println!("Frequency after first 100 runs: {}", sequence.iter().sum::<i32>());
+    println!("First duplicate frequency found: {}", find_first_duplicate_frequency(&sequence));
     
     Ok(())
 }
